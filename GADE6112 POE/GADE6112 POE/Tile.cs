2e89b4f48d81e039f18acf6c1d1cedef59ce4dc6
OmegaOckie <ockiejnr@gmail.com>
2022-09-10 22:17:59 +0200

    Q.2.1
    Created the:
    Tile
    Obstacle
    EmptyTile Class

---
+++ GADE6112 POE/GADE6112 POE/Tile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE
+{
+    internal abstract class Tile
+    {
+        protected int X { get; set; }
+        protected int Y { get; set; }
+
+        public enum TileType
+        {
+            HERO,
+            ENEMY,
+            GOLD,
+            WEAPON
+        }
+
+        public Tile()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
