99813adcc4c302e6c50a70cca77573a4275bd9b1
OmegaOckie <ockiejnr@gmail.com>
2022-09-10 22:25:24 +0200

    Q.2.2
    Added the Character abstract class

---
+++ GADE6112 POE/GADE6112 POE/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE
+{
+    internal abstract class Character : Tile
+    {
+        protected int HP { get; set; }
+        protected int MaxHP { get; set; }
+        protected int Damage { get; set; }
+        protected Tile[] CharacterVision = new Tile[4]; //North, east, south, west
+
+        public enum Movement
+        {
+            NONE,
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+        }
+
+    }
+}
