318d07402c58f787732f89323463c649f82f8750
OmegaOckie <ockiejnr@gmail.com>
2022-09-12 11:16:19 +0200

    Q.2.3
    Finished adding the methods.

--- GADE6112 POE/GADE6112 POE/Character.cs
+++ GADE6112 POE/GADE6112 POE/Character.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GADE6112_POE
 {
     internal abstract class Character : Tile
     {
         protected int HP { get; set; }
-        protected int MaxHP { get; set; }
-        protected int Damage { get; set; }
+        protected int maxHP { get; set; }
+        protected int damage { get; set; }
         protected Tile[] CharacterVision = new Tile[4]; //North, east, south, west
 
         public enum Movement
@@ -26,5 +28,95 @@ namespace GADE6112_POE
         {
 
         }
+
+        /// <summary>
+        /// Attacks a target and decreases
+        /// its health by the attacking character’s damage.
+        /// </summary>
+        /// <param name="target"></param>
+        public virtual void Attack(Character target)
+        {
+            target.HP =- damage;
+        }
+
+        public bool IsDead()
+        {
+            if (HP < 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public virtual bool CheckRange(Character target)
+        {
+            //As of project 1, character is barehanded
+            if (DistanceTo(target) == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Determines the
+        /// absolute distance(number of spaces needed to move – e.g., diagonal is one
+        /// up + one across = 2) between a character and its target.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        private int DistanceTo(Character target)
+        {
+
+            int xDistance = X - target.X;
+            //Convert from - to +
+            if (xDistance < 0)
+            {
+                xDistance = xDistance * -1;
+            }
+
+            int yDistance = Y - target.Y;
+            //Convert from - to +
+            if (yDistance < 0)
+            {
+                yDistance = yDistance * -1;
+            }
+
+            //Addition
+            int distance = xDistance + yDistance;
+
+            return distance;
+        }
+
+        public void Move (Movement ReturnMove)
+        {
+            switch (ReturnMove)
+            {
+                case Movement.NONE:
+                    break;
+                case Movement.UP:
+                    Y = +1;
+                    break;
+                case Movement.DOWN:
+                    Y = -1;
+                    break;
+                case Movement.LEFT:
+                    X = -1;
+                    break;
+                case Movement.RIGHT:
+                    X = +1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public abstract Movement ReturnMove(Movement ReturnMove = 0);
     }
 }
