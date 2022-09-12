cef6891759a93e86711df3a6b33c604e5fa772de
OmegaOckie <ockiejnr@gmail.com>
2022-09-12 11:33:56 +0200

    Q.2.4
    Added the Enemy class

---
+++ GADE6112 POE/GADE6112 POE/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE
+{
+    internal abstract class Enemy : Character
+    {
+        protected int rng;
+
+        public Enemy(int enemyX, int enemyY, int enemyDamage, int enemyCurrentHP ,int enemyMaxHP, char symbol) : base(enemyX, enemyY)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "EnemyClassName at [X, Y] (Amount DMG)";
+        }
+    }
+
+    
+}
---
+++ GADE6112 POE/GADE6112 POE/Swamp Creature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE
+{
+    internal class Swamp_Creature : Enemy
+    {
+        public Swamp_Creature(int scX, int scY,) : base(scX, scY, 1, 10, 10, 'SC')
+        {
+
+        }
+    }
+}
