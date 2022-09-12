3867e16a280204e57ac6bd336c10152d87e9e799
OmegaOckie <ockiejnr@gmail.com>
2022-09-12 12:53:34 +0200

    Q.3.2
    Started with the map

--- GADE6112 POE/GADE6112 POE/Map.cs
+++ GADE6112 POE/GADE6112 POE/Map.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +10,29 @@ namespace GADE6112_POE
 {
     internal class Map
     {
-        private Tile[] map { get; set; }
+        private Tile[,] map { get; set; }
         private Hero hero { get; set; }
-        private Enemy enemy { get; set; }
+        private Enemy[] enemy { get; set; }
 
         //Map width and height
         private int mapWidth;
         private int mapheight;
+
+        private Random rng;
+
+        public Map(int minWidth, int maxWidth, int minHeight, int maxHeight, int numEnemies)
+        {
+            mapWidth = rng.Next(minWidth, maxWidth);
+            mapheight = rng.Next(minHeight, maxHeight);
+
+            map = new Tile[mapWidth, mapheight];
+
+            enemy = new Enemy[numEnemies];
+
+            //Create();
+
+            //UpdateVision();
+        }
 
     }
 }
